8cf7fae9775b3135f901e41aae31385ee0ad89da
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2015-10-01 19:06:01 +0200

    wip tasks collection: fix

--- website/src/models/user.js
+++ website/src/models/user.js
@@ -500,8 +500,7 @@ UserSchema.methods.getTransformedData = function(cb) {
   var self = this;
   this.getTasks(function(err, tasks) {
     if(err) return cb(err);
-    self.addTasksToUser(tasks);
-    cb(null, obj);
+    cb(null, self.addTasksToUser(tasks));
   });
 };
 
